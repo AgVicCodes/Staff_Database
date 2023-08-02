9484c2405a616a3e8ccc1b2b9ffa4d4d4b0993b8
AgVicCodes <victoragbeleye2@gmail.com>
2023-08-02 16:55:50 +0100

    Edited CSS register page

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -1,20 +1,20 @@
-@extends('layouts.app')
-
-@section('content')
-<main class="form-section">
-    <h1 class="header">Register an Account</h1>
-    <form action="POST">
-        <label for="name">Name</label><br>
-        <input type="text" name="name" id="name"><br>
-        
-        <label for="mail">Email</label><br>
-        <input type="text" name="mail" id="mail"><br>
-        
-        <label for="phone">Phone</label><br>
-        <input type="text" name="phone" id="phone"><br>
-        
-        <label for="password">Password</label><br>
-        <input type="text" name="password" id="password"><br>
-    </form>
-</main>
-@endsection
+<x-layout>
+    <section>
+        <main class="form-section">
+            <h1 class="header">Register an Account</h1>
+            <form action="POST">
+                <label for="name">Name</label><br>
+                <input type="text" name="name" id="name"><br>
+                
+                <label for="mail">Email</label><br>
+                <input type="text" name="mail" id="mail"><br>
+                
+                <label for="phone">Phone</label><br>
+                <input type="text" name="phone" id="phone"><br>
+                
+                <label for="password">Password</label><br>
+                <input type="text" name="password" id="password"><br>
+            </form>
+        </main>
+    </section>
+</x-layout>
