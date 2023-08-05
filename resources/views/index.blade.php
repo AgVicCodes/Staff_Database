c6cffa08f6085f8de16b03ad6e7eaff328946066
AgVicCodes <victoragbeleye2@gmail.com>
2023-08-05 21:18:06 +0100

    Moved the CSS to Public folder and cleared cache.

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,56 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
     <link rel="stylesheet" type="text/css" href="{{ asset('css/app.css') }}">
-    {{-- <link rel="stylesheet" href="{{ css.app }}"> !not working --}}
-    {{-- <style>
-        body {
-            background-color: #fcfcfc;
-            color: #353535;
-            font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
-        }
-        
-        ul li a {
-            text-decoration: none;
-            color: #252525;
-        }
-
-        ul li {
-            display: inline;
-            text-transform: uppercase;
-            font-family: 'Candara';
-            margin-left: 12.5px;
-            margin-right: 12.5px;
-        }
-
-        ul li:hover {
-            font-weight: bold;
-        }
-
-        nav ul {
-            margin: 20px !important;
-            padding: 0px !important;
-            /* border: 3px dashed #565656; */
-        }
-
-        nav {
-            /* position: relative;
-            left: 100%;
-            right: 100%;
-            float: left; */
-            display: flex;
-            justify-content: center;
-            /* margin: 100px; */
-            /* border: 10px dashed #cdcddc; */
-
-        }
-
-        header {
-            /* margin: 0;
-            padding: 0; */
-        }        
-    </style> --}}
+    <title>Document</title>
 </head>
 <body>
     <header>
