193a96dc3fd197dfc3576146951049f2065ff0c8
AgVicCodes <victoragbeleye2@gmail.com>
2023-08-15 17:09:44 +0100

    Edited CSS

--- routes/web.php
+++ routes/web.php
@@ -14,10 +14,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/index', function () {
     return view('index');
 });
 
+Route::get('/welcome', function () {
+    return view('index');
+});
+
+Route::get('/about', function () {
+    return view('about');
+});
