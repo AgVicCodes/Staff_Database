abacc4ad8283849204f0b020618c5f34de6b5aeb
AgVicCodes <victoragbeleye2@gmail.com>
2023-08-16 18:02:05 +0100

    Editing pages

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" type="text/css" href="{{ asset('css/app.css') }}">
-    <title>Document</title>
+    <title>@yield('title') - Page</title>
 </head>
 <body>
     <header>
