f05e644a2b2280a97a815d92445d0e9d3f6420dc
AgVicCodes <victoragbeleye2@gmail.com>
2023-08-17 16:23:23 +0100

    Edit Register page

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -4,15 +4,15 @@
     <h1>Register an Account</h1>
     <form action="POST">
         <label for="name">Name</label>
-        <input type="text" name="name" id="name">
+        <input type="text" name="name" id="name"><br>
 
         <label for="mail">Email</label>
-        <input type="text" name="mail" id="mail">
+        <input type="text" name="mail" id="mail"><br>
         
         <label for="phone">Phone</label>
-        <input type="text" name="phone" id="phone">
+        <input type="text" name="phone" id="phone"><br>
         
         <label for="password">Password</label>
-        <input type="text" name="password" id="password">
+        <input type="text" name="password" id="password"><br>
     </form>
 @endsection
