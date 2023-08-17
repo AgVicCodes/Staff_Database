71449d7304c8037b4deb134ba3a4b4d7c6172cad
AgVicCodes <victoragbeleye2@gmail.com>
2023-08-17 16:20:19 +0100

    Registration form

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -1,7 +1,18 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>
-        Register an Account
-    </h1>
+    <h1>Register an Account</h1>
+    <form action="POST">
+        <label for="name">Name</label>
+        <input type="text" name="name" id="name">
+
+        <label for="mail">Email</label>
+        <input type="text" name="mail" id="mail">
+        
+        <label for="phone">Phone</label>
+        <input type="text" name="phone" id="phone">
+        
+        <label for="password">Password</label>
+        <input type="text" name="password" id="password">
+    </form>
 @endsection
