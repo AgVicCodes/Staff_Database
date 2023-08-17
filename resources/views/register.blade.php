4fad6464733ea842ee5ff28e44d418a4784f0d97
AgVicCodes <victoragbeleye2@gmail.com>
2023-08-17 16:37:12 +0100

    Edited Blade

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -1,18 +1,20 @@
 @extends('layouts.app')
 
 @section('content')
+<main class="form-section">
     <h1>Register an Account</h1>
     <form action="POST">
-        <label for="name">Name</label>
+        <label for="name">Name</label><br>
         <input type="text" name="name" id="name"><br>
-
-        <label for="mail">Email</label>
+        
+        <label for="mail">Email</label><br>
         <input type="text" name="mail" id="mail"><br>
         
-        <label for="phone">Phone</label>
+        <label for="phone">Phone</label><br>
         <input type="text" name="phone" id="phone"><br>
         
-        <label for="password">Password</label>
+        <label for="password">Password</label><br>
         <input type="text" name="password" id="password"><br>
     </form>
+</main>
 @endsection
