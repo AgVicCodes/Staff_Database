4a18585ad180dd34e0c69a641a7ffab1d8715b1e
AgVicCodes <victoragbeleye2@gmail.com>
2023-11-28 19:32:01 +0100

    Edited Workspace

--- resources/views/StaffNew.blade.php
+++ resources/views/StaffNew.blade.php
@@ -7,7 +7,35 @@
         <div class="row justify-content-center">
             <div class="col-md-8">
 
-                
+                <form action="/staffNew" method="post" enctype="multipart/form-data">
+                    @csrf
+                    <label for="name" class="mt-4">Name</label>
+                    <input type="text" name="name">
+                    <label for="email" class="mt-4">Email</label>
+                    <input type="text" name="email">
+                    <label for="status" class="mt-4">Status</label>
+                    <input type="text" name="status">
+                    <label for="" class="mt-4"></label>
+                    <input type="text" name="">
+                    <label for="" class="mt-4"></label>
+                    <input type="text" name="">
+                    <label for="" class="mt-4"></label>
+                    <input type="text" name="">
+                    <label for="" class="mt-4"></label>
+                    <input type="text" name="">
+                    <label for="" class="mt-4"></label>
+                    <input type="text" name="">
+                    <label for="" class="mt-4"></label>
+                    <input type="text" name="">
+                    <label for="" class="mt-4"></label>
+                    <input type="text" name="">
+                    <label for="" class="mt-4"></label>
+                    <input type="text" name="">
+                    <label for="" class="mt-4"></label>
+                    <input type="text" name="">
+                    <label for="" class="mt-4"></label>
+                    <input type="text" name="">
+                </form>
                 
             </div>
         </div>
