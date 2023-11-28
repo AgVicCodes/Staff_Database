e06adfc8fb352b1b7ab0637ebb195431d898ced2
AgVicCodes <victoragbeleye2@gmail.com>
2023-11-28 23:06:25 +0100

    Edited Workspace

--- resources/views/StaffNew.blade.php
+++ resources/views/StaffNew.blade.php
@@ -4,19 +4,16 @@
 
 @section('content')
     <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-
+        <div class="row">
+            <div class="col-md-6">
                 <form action="/staffNew" method="post" enctype="multipart/form-data">
                     @csrf
                     <label for="name" class="mt-4">Name</label>
                     <input type="text" name="name">
-                    <label for="email" class="mt-4">Email</label>
-                    <input type="text" name="email">
+
                     <label for="status" class="mt-4">Status</label>
                     <input type="text" name="status">
-                    <label for="second" class="mt-4">Secondary Email</label>
-                    <input type="text" name="second">
+                    
                     <label for="role" class="mt-4">Role</label>
                     <input type="text" name="">
                     <label for="" class="mt-4"></label>
@@ -36,7 +33,41 @@
                     <label for="" class="mt-4"></label>
                     <input type="text" name="">
                 </form>
-                
+            </div>
+            <div class="col-md-6">
+                <form action="/staffNew" method="post" enctype="multipart/form-data">
+                    @csrf
+                    <label for="name" class="mt-4">Name</label>
+                    <input type="text" name="name">
+
+                    <label for="email" class="mt-4">Email</label>
+                    <input type="text" name="email">
+
+                    <label for="status" class="mt-4">Status</label>
+                    <input type="text" name="status">
+
+                    <label for="second" class="mt-4">Secondary Email</label>
+                    <input type="text" name="second">
+                    
+                    <label for="role" class="mt-4">Role</label>
+                    <input type="text" name="">
+                    <label for="" class="mt-4"></label>
+                    <input type="text" name="">
+                    <label for="" class="mt-4"></label>
+                    <input type="text" name="">
+                    <label for="" class="mt-4"></label>
+                    <input type="text" name="">
+                    <label for="" class="mt-4"></label>
+                    <input type="text" name="">
+                    <label for="" class="mt-4"></label>
+                    <input type="text" name="">
+                    <label for="" class="mt-4"></label>
+                    <input type="text" name="">
+                    <label for="" class="mt-4"></label>
+                    <input type="text" name="">
+                    <label for="" class="mt-4"></label>
+                    <input type="text" name="">
+                </form>
             </div>
         </div>
     </div>
