c8021ca7fa37016646102d9be2bafa856b8b853b
AgVicCodes <victoragbeleye2@gmail.com>
2023-11-29 05:06:06 +0100

    Edited Workspace

--- resources/views/StaffNew.blade.php
+++ resources/views/StaffNew.blade.php
@@ -4,71 +4,74 @@
 
 @section('content')
     <div class="container">
-        <div class="row">
-            <div class="col-md-6">
-                <form action="/staffNew" method="post" enctype="multipart/form-data">
-                    @csrf
-                    <label for="name" class="mt-4">Name</label>
-                    <input type="text" name="name">
+        <form action="/staffNew" method="post" enctype="multipart/form-data">
+            @csrf
+            <label for="name" class="mt-4">Name</label>
+            <input type="text" name="name">
+            <br>
+            <label for="status" class="mt-4">Status</label>
+            <input type="text" name="status">
+            <br>
+            <label for="role" class="mt-4">Role</label>
+            <input type="text" name="role">
+            <br>
+            <label for="wage" class="mt-4">Wage</label>
+            <input type="text" name="wage">
+            <br>
+            <label for="hire" class="mt-4">Hire-date</label>
+            <input type="text" name="hire">
+            <br>
+            <label for="" class="mt-4">Field</label>
+            <input type="text" name="">
+            <br>
+            <label for="" class="mt-4"></label>
+            <input type="text" name="">
+            <br>
+            <label for="" class="mt-4"></label>
+            <input type="text" name="">
+            <br>
+            <label for="" class="mt-4"></label>
+            <input type="text" name="">
+            <br>
+            <label for="" class="mt-4"></label>
+            <input type="text" name="">
+            <br>
+            <label for="" class="mt-4"></label>
+            <input type="text" name="">
+            <br>
+        </form>
+        <form action="/staffNew" method="post" enctype="multipart/form-data">
+            @csrf
+            <label for="name" class="mt-4">Name</label>
+            <input type="text" name="name">
 
-                    <label for="status" class="mt-4">Status</label>
-                    <input type="text" name="status">
-                    
-                    <label for="role" class="mt-4">Role</label>
-                    <input type="text" name="">
-                    <label for="" class="mt-4"></label>
-                    <input type="text" name="">
-                    <label for="" class="mt-4"></label>
-                    <input type="text" name="">
-                    <label for="" class="mt-4"></label>
-                    <input type="text" name="">
-                    <label for="" class="mt-4"></label>
-                    <input type="text" name="">
-                    <label for="" class="mt-4"></label>
-                    <input type="text" name="">
-                    <label for="" class="mt-4"></label>
-                    <input type="text" name="">
-                    <label for="" class="mt-4"></label>
-                    <input type="text" name="">
-                    <label for="" class="mt-4"></label>
-                    <input type="text" name="">
-                </form>
-            </div>
-            <div class="col-md-6">
-                <form action="/staffNew" method="post" enctype="multipart/form-data">
-                    @csrf
-                    <label for="name" class="mt-4">Name</label>
-                    <input type="text" name="name">
+            <label for="email" class="mt-4">Email</label>
+            <input type="text" name="email">
 
-                    <label for="email" class="mt-4">Email</label>
-                    <input type="text" name="email">
+            <label for="status" class="mt-4">Status</label>
+            <input type="text" name="status">
 
-                    <label for="status" class="mt-4">Status</label>
-                    <input type="text" name="status">
-
-                    <label for="second" class="mt-4">Secondary Email</label>
-                    <input type="text" name="second">
-                    
-                    <label for="role" class="mt-4">Role</label>
-                    <input type="text" name="">
-                    <label for="" class="mt-4"></label>
-                    <input type="text" name="">
-                    <label for="" class="mt-4"></label>
-                    <input type="text" name="">
-                    <label for="" class="mt-4"></label>
-                    <input type="text" name="">
-                    <label for="" class="mt-4"></label>
-                    <input type="text" name="">
-                    <label for="" class="mt-4"></label>
-                    <input type="text" name="">
-                    <label for="" class="mt-4"></label>
-                    <input type="text" name="">
-                    <label for="" class="mt-4"></label>
-                    <input type="text" name="">
-                    <label for="" class="mt-4"></label>
-                    <input type="text" name="">
-                </form>
-            </div>
-        </div>
+            <label for="second" class="mt-4">Secondary Email</label>
+            <input type="text" name="second">
+            
+            <label for="role" class="mt-4">Role</label>
+            <input type="text" name="">
+            <label for="" class="mt-4"></label>
+            <input type="text" name="">
+            <label for="" class="mt-4"></label>
+            <input type="text" name="">
+            <label for="" class="mt-4"></label>
+            <input type="text" name="">
+            <label for="" class="mt-4"></label>
+            <input type="text" name="">
+            <label for="" class="mt-4"></label>
+            <input type="text" name="">
+            <label for="" class="mt-4"></label>
+            <input type="text" name="">
+            <label for="" class="mt-4"></label>
+            <input type="text" name="">
+            <label for="" class="mt-4"></label>
+            <input type="text" name="">
+        </form>
     </div>
 @endsection
