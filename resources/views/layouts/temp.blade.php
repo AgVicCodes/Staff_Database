085b7ac46b49bcc735a05ca3c30ea2a2ea27d0b3
AgVicCodes <victoragbeleye2@gmail.com>
2023-12-07 11:10:11 +0100

    Edited Workspace

--- resources/views/layouts/temp.blade.php
+++ resources/views/layouts/temp.blade.php
@@ -4,9 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" type="text/css" href="{{ asset('css/app.css') }}">
-    <link rel="stylesheet" type="text/css" href="{{ asset('css/navbar.css') }}">
-    <link rel="stylesheet" type="text/css" href="{{ asset('css/style.css') }}">
+    {{-- <link rel="stylesheet" type="text/css" href="{{ asset('css/app.css') }}"> --}}
+    {{-- <link rel="stylesheet" type="text/css" href="{{ asset('css/navbar.css') }}"> --}}
+    {{-- <link rel="stylesheet" type="text/css" href="{{ asset('css/style.css') }}"> --}}
     <title>EPU-Admin @yield('title')</title>
 </head>
 <body>
