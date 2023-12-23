b90b30d2be9d693bfdb8234ca9c372453644daed
AgVicCodes <victoragbeleye2@gmail.com>
2023-12-23 11:16:06 +0100

    Edited Workspace

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,12 +13,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
-        $this->call([
-            
-            StaffSeeder::class,
-            
-        ]);
+        
+        \App\Models\User::factory(10)->create();
         
     }
 }
